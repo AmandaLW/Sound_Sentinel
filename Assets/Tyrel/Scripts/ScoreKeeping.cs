2da841e070fc64af99a8d8464cda1870a6fb4dfa
Jaxal83 <Jaxal83@users.noreply.github.com>
2018-10-30 00:07:24 -0700

    test updates

---
+++ Assets/Tyrel/Scripts/ScoreKeeping.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeping : MonoBehaviour {
+    private int totalscore = 0;
+    private Text scoreboard;
+
+	// Use this for initialization
+	void Start () {
+        scoreboard = gameObject.GetComponent<Text>();        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreboard.text = string.Format("Score: {0}", totalscore);
+	}
+
+    public void updateScore(int score)
+    {
+        totalscore += score;
+    }
+}
---
+++ Assets/Tyrel/Scripts/ShieldCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShieldCollision : MonoBehaviour {
+
+    ScoreKeeping scoreScript;
+
+	// Use this for initialization
+	void Start () {
+        scoreScript = GameObject.FindGameObjectWithTag("ScoreBoard").GetComponent<ScoreKeeping>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ball")
+        {
+            scoreScript.updateScore(1);
+        }
+    }
+}
