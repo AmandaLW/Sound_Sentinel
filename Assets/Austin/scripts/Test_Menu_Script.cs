2f5316568d452316adb81a3afdab2ba6901f75a5
arpenelerick <42845644+arpenelerick@users.noreply.github.com>
2018-11-03 14:35:12 -0700

    typo correction

--- Assets/Austin/scripts/Test_Menu_Script.cs
+++ Assets/Austin/scripts/Test_Menu_Script.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class Test_Menu_Script : MonoBehaviour {
+        
     public Canvas canvas;
     // Use this for initialization
     void Start() {
         canvasOff();
     }
+
      public void canvasOff()
     {
         canvas = canvas.GetComponent<Canvas>();
