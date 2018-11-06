99b93fe301aac06680e8b1cf85e9a086388c4c84
Jaxal83 <Jaxal83@users.noreply.github.com>
2018-11-05 21:03:02 -0800

    automate testing and implement reports

---
+++ Assets/Tyrel/Scripts/ReportTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReportTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnApplicationQuit()
+    {
+        TestingResults temp = GameObject.FindGameObjectWithTag("Testing").GetComponent<TestingResults>();
+
+        temp.TestRecords(gameObject.name, true);
+    }
+}
