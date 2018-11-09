aaf28bb3c50035dc52865ed929c989dcad26cc77
AmandaLW <43043468+AmandaLW@users.noreply.github.com>
2018-11-08 21:57:45 -0800

    made a cube room in AW Test Scene

---
+++ Assets/Amanda/AW-Scripts/AW_DestroyWalls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AW_DestroyWalls : MonoBehaviour {
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "FloorCube")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Tidy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tidy : MonoBehaviour {
+
+    // Use this for initialization
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "FloorCube")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
