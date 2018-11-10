5bc4293dc97a4d244341f846b6eb99fa5c253fb5
AmandaLW <43043468+AmandaLW@users.noreply.github.com>
2018-11-09 22:53:10 -0800

    Revert "Merge branch 'master' of https://github.com/Jaxal83/Sound_Sentinel"
    This reverts commit d94e2d4746b3c0561d9e145c134f9bac6d942365, reversing
    changes made to 0759ff0d5b19d9f9071515a434066ce652319ae6.

--- Assets/Amanda/AW-Scripts/AW_DestroyWalls.cs
+++ Assets/Amanda/AW-Scripts/AW_DestroyWalls.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AW_DestroyWalls : MonoBehaviour {
-    Score ballsmackin;
 
-    void Start()
-    {
-        ballsmackin = GameObject.FindGameObjectWithTag("ScoreBoard").GetComponent<Score>();
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "FloorCube")
         {
             Destroy(collision.gameObject);
-            ballsmackin.UpdateScore(1);
         }
     }
 }
