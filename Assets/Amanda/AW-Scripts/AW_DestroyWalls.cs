abc5238dfcd3d2b24b287e1742c502517a571fef
Jaxal83 <Jaxal83@users.noreply.github.com>
2018-11-09 21:59:39 -0800

    update

--- Assets/Amanda/AW-Scripts/AW_DestroyWalls.cs
+++ Assets/Amanda/AW-Scripts/AW_DestroyWalls.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AW_DestroyWalls : MonoBehaviour {
+    Score ballsmackin;
 
+    void Start()
+    {
+        ballsmackin = GameObject.FindGameObjectWithTag("ScoreBoard").GetComponent<Score>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "FloorCube")
         {
             Destroy(collision.gameObject);
+            ballsmackin.UpdateScore(1);
         }
     }
 }
