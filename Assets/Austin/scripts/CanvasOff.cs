6811bb9098cf3fa336706292addbbc15b284b233
arpenelerick <42845644+arpenelerick@users.noreply.github.com>
2018-11-27 12:47:41 -0800

    video menu edits

--- Assets/Austin/scripts/CanvasOff.cs
+++ Assets/Austin/scripts/CanvasOff.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Test_Menu_Script : MonoBehaviour {
+public class CanvasOff : MonoBehaviour {
         
     public Canvas canvas;
     // Use this for initialization
--- Assets/Austin/scripts/VideoMenuScript.cs
+++ Assets/Austin/scripts/VideoMenuScript.cs
@@ -17,11 +17,12 @@ public class VideoMenuScript : MonoBehaviour {
         {
             float yCoord = 1.5f;
             float zCoord = 2.5f;
-            yCoord = yCoord + (x * 0.1f);
+            yCoord = yCoord + (x * 0.05f);
             //zCoord = zCoord + (x * 0.1f);
             Transform buttonClone = Instantiate(videoButton, new Vector3(0, yCoord, zCoord), Quaternion.identity, videoCanvas);
             string fileName = filePaths[x].Remove(0, pathLength);
             buttonClone.GetComponentInChildren<Text>().text = fileName;
+            buttonClone.GetComponentInChildren<Text>().tag = "vButton";
             Debug.Log(fileName);
         }
     }
