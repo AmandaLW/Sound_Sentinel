63058f2ed94a1fe5a45d83da131193c4cdc4f5dd
arpenelerick <42845644+arpenelerick@users.noreply.github.com>
2018-11-28 11:55:27 -0800

    Add ability for more file types in the video list

--- Assets/Austin/scripts/VideoMenuScript.cs
+++ Assets/Austin/scripts/VideoMenuScript.cs
@@ -9,24 +9,30 @@ public class VideoMenuScript : MonoBehaviour {
     public Transform videoCanvas;
 	void Start ()
     {
-        string videoFolder = Application.dataPath + "/Tyrel/Videos/";
+        string videoFolder = Application.dataPath + "/Resources/";
+        //To add additional file types to the ingame list place them in this array.
+        string[] fileTypes = {"*.mp4", "*.mp3" };
         int pathLength = (videoFolder.Length);
         string buttonTag = "vButton";
-        Debug.Log(pathLength);
-        string[] filePaths = Directory.GetFiles(videoFolder, "*.mp4");
-        Debug.Log(filePaths[0]);
-        for (int x = 0; x < filePaths.Length; x++)
+        //Debug.Log(pathLength);
+      
+        //Debug.Log(filePaths[0]);
+        float yCoord = 1.5f;
+        float zCoord = 2.5f;
+        for (int k = 0; k < fileTypes.Length; k++)
         {
-            float yCoord = 1.5f;
-            float zCoord = 2.5f;
-            yCoord = yCoord + (x * 0.07f);
-            //zCoord = zCoord + (x * 0.1f);
-            Transform buttonClone = Instantiate(videoButton, new Vector3(0, yCoord, zCoord), Quaternion.identity, videoCanvas);
-            string fileName = filePaths[x].Remove(0, pathLength);
-            buttonClone.GetComponentInChildren<Text>().text = fileName;
-            buttonClone.tag = buttonTag;
-            buttonClone.SendMessage("SetFilePath", filePaths[x]);
-            Debug.Log(fileName);
+            string[] filePaths = Directory.GetFiles(videoFolder, fileTypes[k]);
+            for (int x = 0; x < filePaths.Length; x++)
+            {
+                yCoord = yCoord - 0.07f;
+                //zCoord = zCoord + (x * 0.1f);
+                Transform buttonClone = Instantiate(videoButton, new Vector3(0, yCoord, zCoord), Quaternion.identity, videoCanvas);
+                string fileName = filePaths[x].Remove(0, pathLength);
+                buttonClone.GetComponentInChildren<Text>().text = fileName;
+                buttonClone.tag = buttonTag;
+                buttonClone.SendMessage("SetFilePath", filePaths[x]);
+                Debug.Log(fileName);
+            }
         }
 
         //after all the buttons are created turn them off until use
---
+++ tst/Sound_Sentinel_2018 Nov 28  11.53.41.tst
@@ -0,0 +1,5 @@
+11:53:44 Pause_Menu_Canvas PASS
+11:53:44 MediaScript PASS
+11:53:44 Quad PASS
+11:53:44 [CameraRig] PASS
+11:53:44 Logo PASS
