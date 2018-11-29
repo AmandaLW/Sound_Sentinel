38759bedf1c2cd787395c4a04a2936bea14f1fb9
arpenelerick <42845644+arpenelerick@users.noreply.github.com>
2018-11-29 02:07:33 -0800

    menu color updates

--- Assets/TopScores.cs
+++ Assets/TopScores.cs
@@ -20,20 +20,25 @@ public class TopScores : MonoBehaviour {
         {
             //string line = scoresFromFile;
             int.TryParse(scoresFromFile[x], out scores[x]);
-            Debug.Log("scores converted to ints");
+            //Debug.Log("scores converted to ints");
         }
         Array.Sort(scores);
         float yCoord = 2f;
         float zCoord = 2.5f;
-        for (int k = 0; k < scores.Length; k++)
+        for (int k = 0; k < 10; k++)
         {
-            Debug.Log("Displaying scores");
-            string textString = scores[k].ToString();
+            string textString = scores[(scores.Length - (1 + k))].ToString();
+            Debug.Log(textString);
             Transform displayedScore = Instantiate(textBox, new Vector3(0, yCoord, zCoord), Quaternion.identity, scoreCanvas);
-            textBox.GetComponentInChildren<Text>().text = textString;
-            textBox.GetComponentInChildren<Text>().fontSize = 20;
-            yCoord -= 0.07f;
+            displayedScore.GetComponentInChildren<Text>().text = textString;
+            displayedScore.GetComponentInChildren<Text>().fontSize = 20;
+            yCoord = yCoord - 0.07f;
+            //if (k < 9)
+            //{
+             //   Destroy(displayedScore);
+           // }
         }
+        
         //Reader.Close();
     }
 }
