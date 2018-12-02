6ddffa49a98826cb80d135c71b3acd804dda155c
arpenelerick <42845644+arpenelerick@users.noreply.github.com>
2018-12-02 13:37:48 -0800

    Added hints to main menu

---
+++ Assets/Austin/scripts/Hint_Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Hint_Script : MonoBehaviour {
+   // public Text hintText;
+    public Transform ballText;
+    public Transform hintPanel;
+	// Use this for initialization
+	void SetHintText(Color[] colors)
+    {
+        float xCoord = 0.63f;
+        float yCoord = 1.64f;
+        float zCoord = 1;
+        string textString;
+        for (int x = 0; x < (colors.Length - 1); x++)
+        {
+            textString = "Balls of this color are worth " + (x + 1) + " points.";
+            Transform hintText = Instantiate(ballText, new Vector3(xCoord, yCoord, zCoord), Quaternion.identity, hintPanel);
+            hintText.GetComponentInChildren<Text>().text = textString;
+            hintText.GetComponentInChildren<Text>().color = colors[x];
+            yCoord = yCoord - 0.11f;
+
+        }
+        textString = "Balls of this color explode!;";
+        Transform lastText = Instantiate(ballText, new Vector3(xCoord, yCoord, zCoord), Quaternion.identity, hintPanel);
+        lastText.GetComponentInChildren<Text>().text = textString;
+        lastText.GetComponentInChildren<Text>().color = colors[(colors.Length - 1)];
+    }
+	
+}
--- Assets/Tyrel/Scripts/ColorChange.cs
+++ Assets/Tyrel/Scripts/ColorChange.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ColorChange : MonoBehaviour {
@@ -7,14 +8,25 @@ public class ColorChange : MonoBehaviour {
     Color[] colors = { Color.blue, Color.cyan, Color.green, new Color(255, 165, 0), Color.red, Color.black };
     int count;
     float time = (float)0.5;
+    Scene m_Scene;
+    string mainScene = "Sound_Sentinel";
 
     Renderer ballRenderer;
 
 	// Use this for initialization
 	void Start () {
-        count = 0;
-        ballRenderer = gameObject.GetComponent<Renderer>();
-        ballRenderer.material.color = Color.yellow;
+        m_Scene = SceneManager.GetActiveScene();
+        if (m_Scene.name == mainScene)
+        {
+            count = 0;
+            ballRenderer = gameObject.GetComponent<Renderer>();
+            ballRenderer.material.color = Color.yellow;
+        }
+        else
+        {
+            Debug.Log(m_Scene.name);
+            SendMessage("SetHintText", colors);
+        }
 	}
 	
 	// Update is called once per frame
